3b9b6822d384671f537df9f4446122ebfd2c81dd
chaizz <18037731392@163.com>
2023-03-27 14:11:45 +0800

    扩展User(AbstractUser)

--- base_django_dev_scaffold/settings/base.py
+++ base_django_dev_scaffold/settings/base.py
@@ -124,3 +124,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.TokenAuthentication', ]
 }
+
+
+AUTH_USER_MODEL = "system.User"
