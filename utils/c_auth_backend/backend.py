4df49f4dda660918156b37e3019857dc498c38f8
chaizz <18037731392@163.com>
2023-03-28 11:28:21 +0800

    设置 email、手机号，username 都可以登录

---
+++ utils/c_auth_backend/backend.py
@@ -0,0 +1,30 @@
+"""
+-------------------------------------------------
+    File Name:   backend.py
+    Description: 
+        
+    Author:      chaizz
+    Date:        2023/3/27 17:27
+-------------------------------------------------
+    Change Activity:
+          2023/3/27 17:27
+-------------------------------------------------
+"""
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
+from django.db.models import Q
+
+User = get_user_model()
+
+
+class MyModelBackendBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        if username is None or password is None:
+            return
+        try:
+            user = User.objects.get(
+                Q(username=username) | Q(email=username) | Q(phone_number=username) & Q(is_active=True))
+            if user.check_password(password):
+                return user
+        except Exception as e:
+            return None
