d6ee7c34273f08efba3a79c540cdd1632ab01aed
chaizz <18037731392@163.com>
2023-03-29 18:07:38 +0800

    修改视图响应

--- apps/system/views.py
+++ apps/system/views.py
@@ -1,6 +1,11 @@
 import base64
+from rest_framework.decorators import action
 
 from captcha.views import CaptchaStore, captcha_image
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.exceptions import status
+from utils.c_restframework.c_response import JsonResponse
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -11,7 +16,9 @@ from rest_framework_simplejwt.views import (
 )
 
 from apps.system.models import Users
-from apps.system.serializers import MyTokenObtainPairSerializer, RegisterSerializer
+from apps.system.serializers import MyTokenObtainPairSerializer, RegisterSerializer, PasswordSerializer
+from apps.system.serializers import UserInfoSerializer
+from utils.c_restframework.c_permission import IsAdminPermission
 
 
 class CaptchaAPIView(APIView):
@@ -31,7 +38,7 @@ class CaptchaAPIView(APIView):
             # CaptchaStore.remove_expired()
         except Exception:
             json_data = None
-        return Response(data=json_data)
+        return JsonResponse(data=json_data)
 
 
 class CaptchaRefreshAPIView(APIView):
@@ -51,20 +58,21 @@ class CaptchaRefreshAPIView(APIView):
             # CaptchaStore.remove_expired()
         except Exception:
             json_data = None
-        return Response(data=json_data)
+        return JsonResponse(data=json_data)
 
 
 class LoginTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
     def post(self, request, *args, **kwargs):
-        return super().post(request, *args, **kwargs)
-
+        response = super().post(request, *args, **kwargs)
+        return JsonResponse(response.data)
 
 class DecoratedTokenRefreshView(TokenRefreshView):
 
     def post(self, request, *args, **kwargs):
-        return super().post(request, *args, **kwargs)
+        response = super().post(request, *args, **kwargs)
+        return JsonResponse(response.data)
 
 
 class RegisterView(CreateAPIView):
@@ -72,3 +80,29 @@ class RegisterView(CreateAPIView):
     authentication_classes = ()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+
+class UsersViewSet(ModelViewSet):
+    serializer_class = UserInfoSerializer
+    queryset = Users.objects.all()
+
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.action == 'list':
+            permission_classes = [IsAdminPermission]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
+
+    @action(methods=['post'], detail=True, permission_classes=[],
+            url_path='change_password', url_name='change_password')
+    def set_password(self, request, pk=None, *args, **kwargs):
+        user = self.get_object()
+        serializer = PasswordSerializer(data=request.data)
+        if not serializer.is_valid():
+            raise
+        user.save()
+        return Response(serializer.data)
