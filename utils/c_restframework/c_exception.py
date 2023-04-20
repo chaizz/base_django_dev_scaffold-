c0f0fa0761748d02bfe2d8d106c500ca1cd4ded1
chaizz <18037731392@163.com>
2023-04-20 14:49:05 +0800

    优化异常抛出错误提示

--- utils/c_restframework/c_exception.py
+++ utils/c_restframework/c_exception.py
@@ -11,15 +11,15 @@
 -------------------------------------------------
 """
 
-from utils.c_restframework.c_validator import CustomUniqueValidator, CustomValidationError
-from rest_framework.views import exception_handler
-from utils.c_restframework.c_response import JsonResponse
 from rest_framework.exceptions import ValidationError
+from rest_framework.views import exception_handler
+
+from utils.c_restframework.c_response import JsonResponse
+from utils.c_restframework.c_validator import CustomUniqueValidator, CustomValidationError
 
 
 def custom_exception_handler(exc, context):
-
-    message = '服务器错误'
+    message = '服务器错误！'
 
     response = exception_handler(exc, context)
     # 自定义异常抛出错误！
@@ -32,23 +32,34 @@ def custom_exception_handler(exc, context):
             try:
                 message = exc.detail
             except KeyError:
-                message = "未找到"
+                message = "未找到！"
 
         if response.status_code == 400:
-            message = '输入错误'
+            message = '输入错误！'
 
         elif response.status_code == 401:
-            try:
-                message = exc.default_detail
-            except Exception:
-                message = "认证失败或者Token失效"
+            if hasattr(response, 'data'):
+                data = response.data
+                if isinstance(data, dict) and "detail" in data.keys():
+                    print(data["detail"])
+                    if data.get("detail") == "找不到指定凭据对应的有效用户":
+                        message = "无效的用户名或密码！"
+
+                    if data.get("detail") == "此令牌对任何类型的令牌无效":
+                        message = "认证失败，请重新登录！"
+
+                    if data.get("detail") == "身份认证信息未提供。":  # 身份认证信息未提供。
+                        message = "登录已过期，请重新登录！"
+
+                    if data.get("detail") == "令牌无效或已过期":  # 身份认证信息未提供。
+                        message = "令牌无效或已过期！"
 
         elif response.status_code == 403:
-            message = "没有相应的权限"
+            message = "没有相应的权限！"
 
         elif response.status_code == 405:
-            message = '错误的请求方法'
+            message = '错误的请求方法！'
 
         elif response.status_code >= 500:
-            message = "服务器错误"
+            message = "服务器错误！"
     return JsonResponse(msg=message, code=response.status_code)
